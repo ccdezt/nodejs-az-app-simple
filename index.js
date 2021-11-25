30ca07263b1abd556f0de2c691277caf5fbfc0d8
zer <ZER.THAO@computacenter.com>
2021-11-25 15:28:00 +0100

    block out express

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-var express = require('express');
+/* var express = require('express');
 var server = express();
 var options = {
 index: 'src/index.js'                                             //Fill path here.
 };
 server.use('/', express.static('/home/site/wwwroot', options));
-server.listen(process.env.PORT);
+server.listen(process.env.PORT); */
