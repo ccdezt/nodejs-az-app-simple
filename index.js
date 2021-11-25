071b7195263a5a5a01f91f84cbe03b4cf6bbc1db
zer <ZER.THAO@computacenter.com>
2021-11-25 15:10:16 +0100

    add express

---
+++ index.js
@@ -0,0 +1,7 @@
+var express = require('express');
+var server = express();
+var options = {
+index: 'src/index.js'                                             //Fill path here.
+};
+server.use('/', express.static('/home/site/wwwroot', options));
+server.listen(process.env.PORT);
